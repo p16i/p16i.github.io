005443a272eb6dcf3a936d630b325d40da60eeb0
Pattarawat Chormai <goonnow@rocketmail.com>
2013-09-01 21:41:41 +0700

    1st version of generator.

---
+++ _scripts/generator.rb
@@ -0,0 +1,17 @@
+#!/Users/goonnow/.rbenv/shims/ruby
+
+print <<EOF
+Hey Dude!!!
+Would you like some post from me?
+=================
+EOF
+
+local_path = Dir.getwd
+
+format = 'md'
+date = (Time.new).strftime("%F")
+title = 'Hello-world';
+
+filename = "#{local_path}/_posts/#{date}-#{title}.#{format}"
+puts  filename;
+File.new( filename, "w");
