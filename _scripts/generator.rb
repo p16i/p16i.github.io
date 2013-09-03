663ad714eaa2ab869bdaf8695b5c894e747e8f1b
Pattarawat Chormai <goonnow@rocketmail.com>
2013-09-03 21:51:00 +0700

    Be able to put extension and title.

--- _scripts/generator.rb
+++ _scripts/generator.rb
@@ -1,17 +1,55 @@
 #!/usr/bin/env ruby
 
-print <<EOF
+def main()
+    print <<EOF
 Hey Dude!!!
 Would you like some post from me?
 =================
 EOF
 
-local_path = Dir.getwd
+    local_path = Dir.getwd
 
-format = 'md'
-date = (Time.new).strftime("%F")
-title = 'Hello-world';
 
-filename = "#{local_path}/_posts/#{date}-#{title}.#{format}"
-puts  filename;
-File.new( filename, "w");
+    date = (Time.new).strftime("%F")
+    format = ask_extension();
+    title = ask_title();
+
+
+    filename = "#{date}-#{title}.#{format}"
+    if recv_input("Would you like to create \"#{filename}\"? (y,n): ")[0].eql? "y"
+        actual_path = "#{local_path}/_posts/#{filename}"
+        File.new( actual_path, "w");
+        puts "Now, you get it :D"
+        puts "at \"#{actual_path}\""
+    else
+        puts "I just do nothing :("
+    end
+    #
+    #
+    #
+end
+
+
+def ask_extension()
+    default = "md"
+    question ="Extension (#{default}): "
+    input = recv_input(question);
+    ext = ( input[0] ) ? input : default
+    return ext
+end
+
+def ask_title()
+    default = "new-post"
+    question ="What is `title`? (#{default}): "
+    input = recv_input(question);
+    ext = ( input[0] ) ? input : default
+    return ext
+end
+
+def recv_input(question)
+    print "#{question}"
+    STDOUT.flush
+    return gets.chomp
+end
+
+main();
