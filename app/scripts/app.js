36de7f5fc0b0638330c3d376b671257daed486eb
heytitle <pat.chormai@gmail.com>
2016-07-05 22:54:20 +0200

    update port

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('myApp',['ngRoute', 'ngDisableScroll']);
+
+myApp.config(['$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
+          $locationProvider.hashPrefix('!');
+
+          $routeProvider.
+            when('/', {
+              controller: 'mainController',
+              templateUrl: 'views/index.html'
+            }).
+            when('/portfolio', {
+              controller: 'portfolioController',
+              templateUrl: 'views/portfolio.html'
+            }).
+            otherwise('/');
+        }
+]);
+
+myApp.controller('mainController', function($scope) {
+    $scope.isMain  = true;
+});
