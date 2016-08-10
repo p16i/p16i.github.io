4d950413ee4cab85b9665b7352bcb4299e2d125a
heytitle <pat.chormai@gmail.com>
2016-08-10 22:32:38 +0700

    About me con.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,7 +6,7 @@
 
 /* eslint camelcase: ["error", {properties: "never"}] */
 
-var myApp = angular.module('myApp', ['ngRoute', 'ngDisableScroll']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngDisableScroll', 'd3']);
 
 myApp.config(['$locationProvider', '$routeProvider',
              function config($locationProvider, $routeProvider) {
@@ -79,3 +79,31 @@ myApp.directive('imageOnload', function() {
         }
     };
 });
+
+angular.module('d3', [])
+  .factory('d3Service', ['$document', '$q', '$rootScope',
+    function($document, $q, $rootScope) {
+      var d = $q.defer();
+      function onScriptLoad() {
+        // Load client in the browser
+        $rootScope.$apply(function() { d.resolve(window.d3); });
+      }
+      // Create a script tag with d3 as the source
+      // and call our onScriptLoad callback when it
+      // has been loaded
+      var scriptTag = $document[0].createElement('script');
+      scriptTag.type = 'text/javascript';
+      scriptTag.async = true;
+      scriptTag.src = 'http://d3js.org/d3.v3.min.js';
+      scriptTag.onreadystatechange = function () {
+        if (this.readyState == 'complete') onScriptLoad();
+      }
+      scriptTag.onload = onScriptLoad;
+
+      var s = $document[0].getElementsByTagName('body')[0];
+      s.appendChild(scriptTag);
+
+      return {
+        d3: function() { return d.promise; }
+      };
+}]);
