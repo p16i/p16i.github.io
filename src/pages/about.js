3d420315f935553866658d0d8ceb1a180ade8abc
heytitle <pat.chormai@gmail.com>
2019-09-03 15:12:37 +0200

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,19 @@ import React from "react"
 import SEO from "../components/seo";
 import Layout from "../components/layout";
 
+import IconBosna from "../images/icons/sc-bosna-berlin.png"
+import IconRotation from "../images/icons/sg-rotation-prenzlauer-berg.png"
+import IconPusphaira from "../images/icons/pusphaira.png"
+import OutLink from "../components/outlink";
 
 const Header = ({children}) => {
     return <h3 css={{marginBottom: "20px"}}>
         {children}
     </h3>
+}
+
+const TeamIcon = ({icon}) => {
+    return <img css={{width: "50px", verticalAlign: "middle", marginBottom: "0px", paddingRight: "5px"}} src={icon}/>
 }
 
 const About = () => {
@@ -26,25 +34,21 @@ const About = () => {
     <b>Football</b> is my favourite sport. I've been playing since a young age.
     Although I came to Europe for my study, I still play football there. These are clubs
     that I've played with:
-    <ul>
+    <ul css={{listStyle: "none"}}>
         <li>
-            Pusphaira, 2015
-            Eindhoven, the Netherlands
+            <TeamIcon icon={IconBosna}/><OutLink href="http://www.fussball.de/verein/sc-bosna-berlin-berlin/-/id/01OE5U5FUG000000VV0AG80NVUVDNPJM">SC Bosna Berlin</OutLink>, Germany: Berlin's Keisliga C 2018/19
         </li>
         <li>
-            SG .. , 2015
-            Berlin, Germany
+            <TeamIcon icon={IconRotation}/><OutLink href="">SG Rotitaion Prenzlauer</OutLink>, Berlin Germany: Freizeitliga 2017/2018
         </li>
         <li>
-            SC Bosna Berlin, Keisliga C 2018/19
-            Berlin, Germany
+            <TeamIcon icon={IconPusphaira}/><OutLink href="">Pusphaira 6</OutLink>, Eindhoven, the Netherlands: Sunday Leaguage 2015/2016
         </li>
-
     </ul>
     </p>
     <p>
         I also enjoy <b>Photography</b>, mostly taking b/w photos. Theses are some of my photos.
-        More can been seen on my Instragram: @heytitle.
+        More can been seen on my Instragram: <OutLink href="https://www.instagram.com/heytitle/">@heytitle</OutLink>.
     </p>
   </Layout>
 }
