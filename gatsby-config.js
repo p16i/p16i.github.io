80a41feb8f86af01ac7309a85a72b277349133d5
heytitle <pat.chormai@gmail.com>
2019-09-04 00:05:25 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Pattarawat Chormai`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A PhD Candidate at Max Planck School of Cognition`,
+    author: `@heytitle`,
   },
   plugins: [
     `gatsby-plugin-emotion`,
